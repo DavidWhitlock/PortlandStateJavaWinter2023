651be20df00c62547c818d6616cfb2b4aea1c967
David Whitlock <david.m.whitlock@gmail.com>
2023-03-08 18:13:35 -0800

    Added a simple button that displays a Toast.

--- airline-android/app/src/main/java/edu/pdx/cs410J/whitlock/MainActivity.java
+++ airline-android/app/src/main/java/edu/pdx/cs410J/whitlock/MainActivity.java
@@ -3,6 +3,8 @@ package edu.pdx.cs410J.whitlock;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void displayMessage(View view) {
+        Toast.makeText(this, "Hello everyone", Toast.LENGTH_SHORT).show();
     }
 }
