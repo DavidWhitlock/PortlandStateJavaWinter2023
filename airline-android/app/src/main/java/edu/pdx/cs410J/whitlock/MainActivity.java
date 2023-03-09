b1dccf1e6d6b0d2798099b7e902d46eb72632a81
David Whitlock <david.m.whitlock@gmail.com>
2023-03-08 18:52:46 -0800

    Read in the values from the text fields and display the sum.

--- airline-android/app/src/main/java/edu/pdx/cs410J/whitlock/MainActivity.java
+++ airline-android/app/src/main/java/edu/pdx/cs410J/whitlock/MainActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -17,5 +19,22 @@ public class MainActivity extends AppCompatActivity {
     public void displayMessage(View view) {
         Flight flight = new Flight();
         Toast.makeText(this, flight.toString(), Toast.LENGTH_LONG).show();
+    }
+
+    public void computeSum(View view) {
+        EditText leftOperandEditText = findViewById(R.id.leftOperand);
+        EditText rightOperandEditText = findViewById(R.id.rightOperand);
+
+        String leftOperandString = leftOperandEditText.getText().toString();
+        String rightOperandString = rightOperandEditText.getText().toString();
+
+        int leftOperand = Integer.parseInt(leftOperandString);
+        int rightOperand = Integer.parseInt(rightOperandString);
+
+        int sum = leftOperand + rightOperand;
+
+        EditText sumEditText = findViewById(R.id.sum);
+        sumEditText.setText(String.valueOf(sum), TextView.BufferType.EDITABLE);
+
     }
 }
