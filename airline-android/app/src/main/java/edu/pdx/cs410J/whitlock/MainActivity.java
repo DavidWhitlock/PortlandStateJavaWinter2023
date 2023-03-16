3d5a978b5fdb731f4ae44146bc5788e8317bc248
David Whitlock <david.m.whitlock@gmail.com>
2023-03-15 18:37:12 -0700

    Add a ListView to the MainActivity and populate it with an ArrayAdapter.

--- airline-android/app/src/main/java/edu/pdx/cs410J/whitlock/MainActivity.java
+++ airline-android/app/src/main/java/edu/pdx/cs410J/whitlock/MainActivity.java
@@ -4,8 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.AndroidException;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -15,6 +18,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ListView listOfSums = findViewById(R.id.sums);
+        Integer[] numbers = { 1, 2, 3, 4};
+        ArrayAdapter<Integer> sums = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, numbers);
+        listOfSums.setAdapter(sums);
     }
 
     public void launchCalculator(View view) {
